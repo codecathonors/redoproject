9edc41e5a6e202b90375362252d52cc5ec985004
CatherineHonors <cathonors@gmail.com>
2022-03-24 16:19:57 -0400

    event listeners in cooper page

--- script.js
+++ script.js
@@ -3,19 +3,8 @@ h2.textContent = "Yes, character stats as in a video game...";
 
 document.querySelector("body").appendChild(h2);
 
-const coopevent1 = document.getElementById("coopevent1");
+const coopevent1 = document.querySelector(".coopevent1")
 
-coopevent1.addEventListener("click", addText);
-
-function addText() {
-    coopevent1.textContent = "Cooper is a Brussels Griffon/Shih Tzu mix, also known as a 'Shiffon.'"
-}
-
-const coopevent2 = document.getElementById("coopevent2");
-
-coopevent2.addEventListener("click", addText);
-
-function addText() {
-    coopevent2.textContent = "The Brussel Griffon breed is what George Lucas based the Ewoks on!"
-}
-    
+coopevent1.addEventListener('click', function(){
+    alert('Cooper\'s birthday is April 1st!');
+})
